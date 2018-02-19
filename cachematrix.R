bd34ef6dd4735d3e810ca476c3f346ab93f96cdb
himanshur04 <himanshu.rc@gmail.com>
2018-02-18 22:18:47 -0800

    [updated function]

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix function creates and returns a list of functions
+## which are used by cacheSolve to get/set the invert of the matrix (in cache) 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    #initializing the cache variable to NULL 
+    p <- NULL
+    
+    # set/create a matrix in workign environment
+    set <- function(y) {
+            x <<- y
+            p <<- NULL
+    }
+    get <- function() x
+    # setMatrix will invert the matrix and stores it in cache (p)
+    setMatrix <- function(inv) p <<- inv
+    # retrives the inverted matrix from cache (p)
+    getInv <- function() p
+    
+    #list containing created functions
+    list(set = set, get = get,
+         setMatrix = setMatrix,
+         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function calculates the inverse of the matrix created
+## above. If the inverted matrix is not present, it will create it in
+## in the workign environment and stores it in cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+              # getInv get the Inverse of the matrix
+              p <- x$getInv()
+              
+              # gets cached matrix if it exists else creates it
+              if(!is.null(p)) {
+                message("getting cached matrix")
+                return(p)
+              }
+              
+              # matrix creation
+              matrix_hr <- x$get()
+              #Solve function inverse the square matrix
+              p <- solve(matrix_hr, ...)
+              x$setMatrix(p)
+              return(p)
 }
